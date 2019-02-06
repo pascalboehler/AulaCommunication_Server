01ff3826629fd1e91159dbbd7a58725e4843fdf4
pascalboehler <pascal.boehler@hotmail.de>
2019-02-06 20:34:10 +0100

    Added multi threading to server

--- Source/main.py
+++ Source/main.py
@@ -3,27 +3,75 @@
 # 06/02/2019 15:33            #
 ###############################
 
+# imoprt socjet programming library
 import socket
-# create a socket object
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# get local machine name
-host = socket.gethostname()
+# import thread module
+from _thread import *
+import threading
+print_lock = threading.Lock()
 
-port = 8080
+# Public variables
+connectionList = []
 
-# bind to the port
-serversocket.bind((host, port))
+# thread function
+def threaded(c):
+    while True:
+        # data received from client
+        data = c.recv(1024)
+        if not data:
+            print('Bye')
 
-#queue up to 5 requests
-serversocket.listen(5)
+            # lock released on exit
+            print_lock.release()
+            break
 
-while True:
-    #establish a connection
-    clientsocket, addr = serversocket.accept()
 
-    print("Got a connection from %s" % str(addr))
+        # Append data to message list and send message to all other clients
+        sendToOthers(data)
 
-    msg = "Thank you for connecting" + "\r\n"
-    clientsocket.send(msg.encode('ascii'))
-    clientsocket.close()
+
+    c.close()
+
+
+def Main():
+    host = ""
+
+    # reverse a port on cour computer
+    # in our case it is 8080 but it
+    # can be anything
+    port = 8080
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((host, port))
+    print("socket binded to port", port)
+
+    # put the socket into listening mode
+    s.listen(5)
+    print("socket is listening")
+
+    # a forever loop until client wants to exit
+    while True:
+
+        # establish connection with client
+        c, addr = s.accept()
+
+        # lock acquired by client
+        print_lock.acquire()
+        print("Connected to: ", addr[0], ":", addr[1])
+
+        connectionList.append(c)
+
+        # Start a new thread and return its identifier
+        start_new_thread(threaded, (c,))
+
+    s.close()
+
+
+def sendToOthers(data):
+    for element in connectionList:
+        element.send(data)
+
+
+
+if __name__ == '__main__':
+    Main()
