8a99d990f144836d36722a5d1bd317dbd8e020db
pascalboehler <pascal.boehler@hotmail.de>
2019-02-07 13:29:45 +0100

    - Added multiple connections to server - Needs server close method which is called after list is empty

--- Source/main.py
+++ Source/main.py
@@ -3,75 +3,62 @@
 # 06/02/2019 15:33            #
 ###############################
 
-# imoprt socjet programming library
-import socket
+# Imports
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
 
-# import thread module
-from _thread import *
-import threading
-print_lock = threading.Lock()
+clients = {}
+addresses = {}
 
-# Public variables
-connectionList = []
+HOST = ''
+PORT = 33000
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+SERVER = socket(AF_INET, SOCK_STREAM)
+SERVER.bind(ADDR)
 
-# thread function
-def threaded(c):
+
+def accept_incoming_connections():
+    """Sets up handling for incoming clients"""
     while True:
-        # data received from client
-        data = c.recv(1024)
-        if not data:
-            print('Bye')
+        client, client_addr = SERVER.accept()
+        print("%s:%s has connected." % client_addr)
+        client.send(bytes("Greetings from the cave!" + "Now type ypur name and press enter!", "utf8"))
+        addresses[client] = client_addr
+        Thread(target=handle_client, args=(client,)).start()
 
-            # lock released on exit
-            print_lock.release()
+
+def handle_client(client):
+    # Takes client socket argument.
+    name = client.recv(BUFSIZ).decode("utf8")
+    welcome = 'Welcome %s! If you ever want to quit, type {quit} to exit.' % name
+    client.send(bytes(welcome, "utf8"))
+    msg = "%s has joined the chat!" % name
+    broadcast(bytes(msg, "utf8"))
+    clients[client] = name
+    while True:
+        msg = client.recv(BUFSIZ)
+        if msg != bytes("{quit}", "utf8"):
+            broadcast(msg, name+": ")
+        else:
+            client.send(bytes("{quit}", "utf8"))
+            client.close()
+            del clients[client]
+            broadcast(bytes("%s has left the chat." % name, "utf8"))
             break
 
 
-        # Append data to message list and send message to all other clients
-        sendToOthers(data)
+def broadcast(msg, prefix=""):
+    # prefix is for name identification
+    """Broadcasts a message to all the clients"""
+    for sock in clients:
+        sock.send(bytes(prefix, "utf8")+msg)
 
 
-    c.close()
-
-
-def Main():
-    host = ""
-
-    # reverse a port on cour computer
-    # in our case it is 8080 but it
-    # can be anything
-    port = 8080
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((host, port))
-    print("socket binded to port", port)
-
-    # put the socket into listening mode
-    s.listen(5)
-    print("socket is listening")
-
-    # a forever loop until client wants to exit
-    while True:
-
-        # establish connection with client
-        c, addr = s.accept()
-
-        # lock acquired by client
-        print_lock.acquire()
-        print("Connected to: ", addr[0], ":", addr[1])
-
-        connectionList.append(c)
-
-        # Start a new thread and return its identifier
-        start_new_thread(threaded, (c,))
-
-    s.close()
-
-'''
-def sendToOthers(data):
-    for element in connectionList:
-        element.send(data)
-
-'''
-
-if __name__ == '__main__':
-    Main()
+if __name__ == "__main__":
+    SERVER.listen(5) # Listens for 5 connections at max.
+    print("Waiting for connection...")
+    ACCEPT_THREAD = Thread(target=accept_incoming_connections)
+    ACCEPT_THREAD.start() # Starts the infinite loop.
+    ACCEPT_THREAD.join()
+    SERVER.close()
